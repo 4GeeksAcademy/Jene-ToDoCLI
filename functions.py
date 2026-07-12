089139896ec034426eb39ec1bd99d5bd23aa2565
JeneJohn <jenefajohn.me@gmail.com>
2026-07-12 05:53:57 +0000

    function added

--- functions.py
+++ functions.py
@@ -1,9 +1,8 @@
-def create_tasks(task_name:str,person:str,status:str,priority:str,id:int):
+def create_task(task_name:str,person:str,priority:str,id:int):
     return {
         "id" : id,
         "task_name" :task_name,
         "person" : person,
-        "status" : status,
         "priority" : priority
 
     }
--- todos.py
+++ todos.py
@@ -1,40 +1,59 @@
 # Sherwood Logistics
 
+from functions import create_task
 last_id: int = 0
-tasks:list[str,str|int] =[]
+tasks:dict[int, dict[str, str | int]] ={}
 while True:
-    print(" ")
-    print(" ")
+    print("\n")
     print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
     print(" >>>----->   SHERWOOD Logistics   <-----<<< ")
     print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-    print(" ")
-    print("1. Create Task")
+    print("\n1. Create Task")
     print("2. List Task")
     print("3. Delete Task")
     print("q. Quit")
-    choice:str=input("Make your selection:")
+    choice:str=input("\nPick a number:")
     if choice == "1":
-        print("Create new Mission - Add task")
-        task_name:str=input("Enter task name: ")
+        print("\nCreate new Mission (Add task)")
+        task_name:str=input("\nEnter task name: ")
         person:str=input("Enter person assigned:")
-        status:str=input("Enter status of task: ")
         priority:str=input("Enter priority of task:")
         last_id += 1
-        id = last_id
-        tasks.append(create_task(
-            task_name,person,status,priority,id
-        ))
-        print(f"{task_name} added to tasks.\n\n")
-    elif choice == "2":
-        print(" ")   
-        print("Review Mission - List task")
+        task_id = last_id
+        tasks[id] = create_task(
+            task_name,person,priority,id
+        )
+        print(f"\n Task Name :{task_name} is now added to tasks.\n")
+    elif choice == "2":   
+        print("Review Mission (List task details)")
+        lookup_id:int = input("Enter task ID: ")
+
+        if not lookup_id :
+            for task in tasks.values():
+                print(f"{task['id']}:{task['task_name']} - {task['person']}")
+            continue
+
+        if int(lookup_id) not in tasks.keys():
+            print(f"Task {lookup_id} not found")
+            continue
+
+        task = tasks[int(lookup_id)]
+        print(f"{task['id']}:{task['task_name']} - {task['person']}")
+
+
     elif choice == "3":
-        print(" ")
-        print("Mission Accomplished / Aborted - Delete Task")
+        print("Mission Accomplished / Aborted (Delete Task)")
+        
+        lookup_id:int = input("Enter task ID to delete: ")
+        if int(lookup_id) not in tasks.keys():
+            print(f"Task {lookup_id} not found")
+            continue 
+        if input("Are you sure y/n ?").lower() == "y" :
+            del tasks[int(lookup_id)]
+
     elif choice == "q" or choice == "Q":
         print(" ")
-        print("Unstring bows - Quit")
+        print("Unstring bows (Quit) ")
         quit()
 
 
